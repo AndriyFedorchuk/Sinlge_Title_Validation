438e2990d3d01b01df11a81c2a0fa74ddf2b2a7a
AndriyFedorchuk <andriy@flatterthanearth.com>
2015-08-22 17:47:26 -0700

    Homework 33 Modifing Single_Title_Validation

--- src/main/java/core/Single_Title_Validation.java
+++ src/main/java/core/Single_Title_Validation.java
@@ -7,12 +7,19 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Single_Title_Validation {
 	public static void main(String[] args) {
-
+		if (args.length == 0){
+			System.err.println("No arguments!");
+			System.exit(1);
+		}
 		WebDriver driver = new FirefoxDriver();    // Version 1.1 :: Firefox
 
 	
-		String url = "http://www.learn2test.net";
-		String title_expected = "learn2test.net";
+		String param[] = args[0].split("\\|");
+		
+		String url = param[0];
+		
+		String title_expected = param[1];
+
 		
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
